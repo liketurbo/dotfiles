9822b01fe489b65cc29950de11d8581bb10f83a8
Rami Chasygov <4y9hx1bs1@mozmail.com>
2022-11-06 15:12:39 +0300

    feat: highlight yanked region

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -4,3 +4,9 @@ vim.wo.relativenumber = true
 
 -- Use global clipboard
 vim.opt.clipboard = 'unnamedplus'
+
+-- Highlight yank
+vim.api.nvim_command('augroup highlight_yank')
+vim.api.nvim_command('autocmd!')
+vim.api.nvim_command('au TextYankPost * silent! lua vim.highlight.on_yank { higroup=\'IncSearch\', timeout=200 }')
+vim.api.nvim_command('augroup END')
