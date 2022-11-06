467afa72eddaf1de11f63831d5a7530c0a8dd0a1
Rami Chasygov <4y9hx1bs1@mozmail.com>
2022-11-06 15:17:47 +0300

    feat: select all shortcut

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,11 +1,6 @@
-local map = vim.api.nvim_set_keymap
-local opts = {noremap = true, silent = true}
-
--- Do not yank on x
-map('n', 'x', '"_x', opts)
-
 -- Increment/decrement
-map('n', '+', '<C-a>', opts)
-map('n', '-', '<C-x>', opts)
-
 vim.keymap.set('n', '+', '<C-a>')
+vim.keymap.set('n', '-', '<C-x>')
+
+-- Select all
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
