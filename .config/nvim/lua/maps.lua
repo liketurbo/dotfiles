5da0d4c642a046ef285b4113eb5b4933c09df44b
Rami Chasygov <4y9hx1bs1@mozmail.com>
2022-11-07 18:04:21 +0300

    feat: add fzf

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -4,3 +4,6 @@ vim.keymap.set('n', '-', '<C-x>')
 
 -- Select all
 vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- Toggle between buffers
+vim.keymap.set('n', '<Leader><Leader>', '<C-^>')
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -8,19 +8,6 @@ return require('packer').startup(function(use)
   -- Autocompletion
   use {'neoclide/coc.nvim', branch = 'release'}
 
-  -- Easymotion
-  use 'easymotion/vim-easymotion'
-  -- ... find a word
-  vim.keymap.set('n', '<leader>w', '<Plug>(easymotion-w)')
-  vim.keymap.set('n', '<leader>W', '<Plug>(easymotion-b)')
-  vim.keymap.set('v', '<leader>w', '<Plug>(easymotion-w)')
-  vim.keymap.set('v', '<leader>W', '<Plug>(easymotion-b)')
-  -- ... find a line
-  vim.keymap.set('n', 'K', '<Plug>(easymotion-k)')
-  vim.keymap.set('n', 'J', '<Plug>(easymotion-j)')
-  vim.keymap.set('v', 'K', '<Plug>(easymotion-k)')
-  vim.keymap.set('v', 'J', '<Plug>(easymotion-j)')
-
   -- Theme
   use 'ayu-theme/ayu-vim'
   -- ... config
@@ -30,17 +17,18 @@ return require('packer').startup(function(use)
   -- Bottom line
   use 'itchyny/lightline.vim'
 
-  -- File explorer
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
-    requires = { {'nvim-lua/plenary.nvim'} }
+  -- Fuzzy finder
+  use {'junegunn/fzf', run = './install --bin'}
+  use {'ibhagwan/fzf-lua',
+    -- ... icon support
+    requires = {'kyazdani42/nvim-web-devicons'}
   }
-  local builtin = require('telescope.builtin')
-  vim.keymap.set('n', '<leader>f', builtin.find_files, {})
-  vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-  vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-  vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
-
-  -- Icons for file explorer
-  use 'nvim-tree/nvim-web-devicons'
+  vim.keymap.set('n', '<C-p>',
+      "<cmd>lua require('fzf-lua').files()<CR>",
+      {noremap = true, silent = true}
+  )
+  vim.keymap.set('n', '<leader>;',
+      "<cmd>lua require('fzf-lua').buffers()<CR>", 
+      {noremap = true, silent = true}
+  )
 end)
