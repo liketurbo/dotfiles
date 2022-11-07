6eb96552234b17a62b9acaa1c44cce6ecdfacf30
Rami Chasygov <4y9hx1bs1@mozmail.com>
2022-11-07 18:12:59 +0300

    feat: change easymotion maps

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -23,6 +23,7 @@ return require('packer').startup(function(use)
     -- ... icon support
     requires = {'kyazdani42/nvim-web-devicons'}
   }
+  -- ... custom keymaps
   vim.keymap.set('n', '<C-p>',
       "<cmd>lua require('fzf-lua').files()<CR>",
       {noremap = true, silent = true}
@@ -31,4 +32,18 @@ return require('packer').startup(function(use)
       "<cmd>lua require('fzf-lua').buffers()<CR>", 
       {noremap = true, silent = true}
   )
+
+  -- Easymotion
+  use 'easymotion/vim-easymotion'
+  vim.keymap.set('n', '<leader>', '<Plug>(easymotion-prefix)')
+  -- ... find a word
+  vim.keymap.set('n', 'f', '<Plug>(easymotion-w)')
+  vim.keymap.set('n', 'F', '<Plug>(easymotion-b)')
+  vim.keymap.set('v', 'f', '<Plug>(easymotion-w)')
+  vim.keymap.set('v', 'F', '<Plug>(easymotion-b)')
+  -- ... find a line
+  vim.keymap.set('n', 'K', '<Plug>(easymotion-k)')
+  vim.keymap.set('n', 'J', '<Plug>(easymotion-j)')
+  vim.keymap.set('v', 'K', '<Plug>(easymotion-k)')
+  vim.keymap.set('v', 'J', '<Plug>(easymotion-j)')
 end)
