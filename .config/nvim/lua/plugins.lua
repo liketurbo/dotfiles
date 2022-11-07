15a780918d9bda70be18d8aca9e1ad269e0e9e89
Rami Chasygov <4y9hx1bs1@mozmail.com>
2022-11-07 13:26:50 +0300

    feat: add nvim telescope

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -29,4 +29,15 @@ return require('packer').startup(function(use)
 
   -- Bottom line
   use 'itchyny/lightline.vim'
+
+  -- File explorer
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  local builtin = require('telescope.builtin')
+  vim.keymap.set('n', '<leader>f', builtin.find_files, {})
+  vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+  vim.keymap.set('n', '<leader>b', builtin.buffers, {})
+  vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
 end)
