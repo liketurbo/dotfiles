8df8c7512653f6b91b270c901f8807c0dadab3db
Rami Chasygov <4y9hx1bs1@mozmail.com>
2022-11-07 20:32:59 +0300

    refactor: add empty lines

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -10,6 +10,7 @@ return require('packer').startup(function(use)
 
   -- Theme
   use 'ayu-theme/ayu-vim'
+
   -- ... config
   vim.g.ayucolor = 'dark'
   vim.cmd "colorscheme ayu"
@@ -20,9 +21,11 @@ return require('packer').startup(function(use)
   -- Fuzzy finder
   use {'junegunn/fzf', run = './install --bin'}
   use {'ibhagwan/fzf-lua',
+
     -- ... icon support
     requires = {'kyazdani42/nvim-web-devicons'}
   }
+
   -- ... custom keymaps
   vim.keymap.set('n', '<C-p>',
       "<cmd>lua require('fzf-lua').files()<CR>",
@@ -35,12 +38,16 @@ return require('packer').startup(function(use)
 
   -- Easymotion
   use 'easymotion/vim-easymotion'
+
+  -- ... disable <leader><leader>
   vim.keymap.set('n', '<leader>', '<Plug>(easymotion-prefix)')
+
   -- ... find a word
   vim.keymap.set('n', 'f', '<Plug>(easymotion-w)')
   vim.keymap.set('n', 'F', '<Plug>(easymotion-b)')
   vim.keymap.set('v', 'f', '<Plug>(easymotion-w)')
   vim.keymap.set('v', 'F', '<Plug>(easymotion-b)')
+
   -- ... find a line
   vim.keymap.set('n', 'K', '<Plug>(easymotion-k)')
   vim.keymap.set('n', 'J', '<Plug>(easymotion-j)')
