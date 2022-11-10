c654f2cd3cb8466511f141c6f2dffa15dd056052
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-10 23:00:29 +0300

    feat: autocomplete with tab instead of space

--- .config/nvim/after/plugin/autocomplete.lua
+++ .config/nvim/after/plugin/autocomplete.lua
@@ -8,22 +8,20 @@ cmp.setup({
   mapping = {
     ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    ['<Tab>'] = cmp.mapping.select_next_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
+    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm({
+    ['<Tab>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     })
   },
   sources = {
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-    { name = 'path' },
-    { name = 'buffer' },
-    { name = 'nvim_lsp_signature_help' },
+    {name = 'nvim_lsp'},
+    {name = 'vsnip'},
+    {name = 'path'},
+    {name = 'buffer'},
+    {name = 'nvim_lsp_signature_help'},
   },
 })
