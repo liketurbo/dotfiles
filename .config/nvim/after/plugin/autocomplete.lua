45eaff4bc367ca1229f2c40f7a32c5e92d460dfc
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-14 13:44:09 +0300

    feat: change keys for autocomplete

--- .config/nvim/after/plugin/autocomplete.lua
+++ .config/nvim/after/plugin/autocomplete.lua
@@ -8,11 +8,15 @@ cmp.setup({
   mapping = {
     ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
+    ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4)), 
+    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4)),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
     ['<C-e>'] = cmp.mapping.close(),
     ['<Tab>'] = cmp.mapping.confirm({
+      behavior = cmp.ConfirmBehavior.Insert,
+      select = true,
+    }),
+    ['<CR>'] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Insert,
       select = true,
     })
