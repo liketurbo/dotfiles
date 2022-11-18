3acb35ee8fdc2fee326823339dba2542b6b29b23
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-18 18:58:22 +0300

    refactor: remove some maps

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,15 +1,5 @@
--- Increment/decrement
-vim.keymap.set('n', '+', '<C-a>')
-vim.keymap.set('n', '-', '<C-x>')
-
--- Select all
-vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
-
 -- Create a new file in the current directory
-vim.cmd('nnoremap <leader>o :e <C-R>=expand("%:p:h") . "/" <CR>')
-
--- Paste with <C-v>
-vim.keymap.set('i', '<C-v>', '<C-r>"')
+vim.keymap.set('n', '<leader>o', ':e <C-R>=expand("%:p:h") . "/" <CR>')
 
 -- Save a file
 vim.keymap.set('n', '<leader>w', ':w<CR>')
