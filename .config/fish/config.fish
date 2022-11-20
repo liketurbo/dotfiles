be14ecdbd4c1428f715acfe18b5c648a72043e97
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-20 16:14:25 +0300

    feat: add keybinding for jk

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,3 +9,7 @@ alias ll="exa -l --icons"
 alias lla="ll -a"
 alias vim="nvim"
 alias cls="clear"
+
+# Exit the insert mode with jk
+bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+bind -M insert kj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
