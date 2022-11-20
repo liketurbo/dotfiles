1c84c613626c77d8cc94a636220e5571888f8e96
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-20 23:36:56 +0300

    refactor: use leader

--- .config/nvim/after/plugin/lsp-rust.lua
+++ .config/nvim/after/plugin/lsp-rust.lua
@@ -1,10 +1,10 @@
 local rt = require('rust-tools')
 
 local opts = { noremap=true, silent=true }
-vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
 local opts = {
   -- rust-tools options
