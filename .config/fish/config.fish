56030625fa73574b30136a2c69094b7e94b76871
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-22 00:06:50 +0300

    refactor: shorter comments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
-# Suppress the greeting message
+# No greeting
 set fish_greeting
 
-# Change keybindings to vi
+# Use vi
 fish_vi_key_bindings
 
 # Aliases
@@ -10,6 +10,6 @@ alias lla="ll -a"
 alias vim="nvim"
 alias cls="clear"
 
-# Exit the insert mode with jk
+# JK exit
 bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 bind -M insert kj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
