6b6d9281c21d195b5f362f4dbd27bff8d9098f55
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-22 00:22:13 +0300

    refactor: format file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,66 +2,53 @@
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'  
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-  use 'folke/tokyonight.nvim'
-  
-  -- Bottom line
-  use 'itchyny/lightline.vim'
+	-- Theme
+	use 'folke/tokyonight.nvim'
 
-  -- Fuzzy finder
-  use 'airblade/vim-rooter'
-  use {'junegunn/fzf', run = './install --bin'}
-  use {'ibhagwan/fzf-lua',
-    -- ... icon support
-    requires = 'kyazdani42/nvim-web-devicons'
-  }
+	-- Bottom line
+	use 'itchyny/lightline.vim'
 
-  -- NVim LSP configuration
-  use 'neovim/nvim-lspconfig'
+	-- LSP
+	use 'neovim/nvim-lspconfig'
 
-  -- Rust LSP
-  use 'simrat39/rust-tools.nvim'
+	-- Visualize lsp progress
+	use({
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup()
+		end
+	})
 
-  -- Show state of the progress
-  use {
-    'j-hui/fidget.nvim',
-    config = function()
-      require'fidget'.setup {}
-    end
-  }
-  
-  -- Highlight trouble lines
-  use {
-    'folke/trouble.nvim',
-    config = function()
-      require'trouble'.setup {}
-    end
-  }
+	-- Treesitter
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = ':TSUpdate'
+	}
 
-  -- Autocomplete
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-nvim-lsp-signature-help'
-  use 'hrsh7th/cmp-path'
-  use 'hrsh7th/cmp-vsnip'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/vim-vsnip'
-  use 'hrsh7th/vim-vsnip-integ'
+	-- GitHub Copilot
+	use 'github/copilot.vim'
 
-  -- Treesitter
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
-
-  -- GitHub Copilot
-  use 'github/copilot.vim'
-	-- File explorer
+	-- Telescope
 	use {
 		'nvim-telescope/telescope.nvim', tag = '0.1.0',
 		requires = { { 'nvim-lua/plenary.nvim' } }
 	}
+
 	use { "nvim-telescope/telescope-file-browser.nvim" }
+
+	-- Rust LSP
+	use 'simrat39/rust-tools.nvim'
+
+	-- Autocomplete
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-nvim-lsp'
+	use 'hrsh7th/cmp-nvim-lsp-signature-help'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-vsnip'
+	use 'hrsh7th/nvim-cmp'
+	use 'hrsh7th/vim-vsnip'
+	use 'hrsh7th/vim-vsnip-integ'
 end)
