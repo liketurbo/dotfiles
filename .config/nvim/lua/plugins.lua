213fbad403e331edd418ab941a94a29660f1ca1a
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-22 01:05:09 +0300

    refactor: change cmp file name

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,53 +2,50 @@
 vim.cmd [[packadd packer.nvim]]
 
 require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-	-- Theme
-	use 'folke/tokyonight.nvim'
+  -- Theme
+  use 'folke/tokyonight.nvim'
 
-	-- Bottom line
-	use 'itchyny/lightline.vim'
+  -- Bottom line
+  use 'itchyny/lightline.vim'
 
-	-- LSP
-	use 'neovim/nvim-lspconfig'
+  -- LSP
+  use 'neovim/nvim-lspconfig'
 
-	-- Visualize lsp progress
-	use({
-		"j-hui/fidget.nvim",
-		config = function()
-			require("fidget").setup()
-		end
-	})
+  -- Visualize lsp progress
+  use({
+    'j-hui/fidget.nvim',
+    config = function()
+      require('fidget').setup()
+    end
+  })
 
-	-- Treesitter
-	use {
-		'nvim-treesitter/nvim-treesitter',
-		run = ':TSUpdate'
-	}
+  -- Treesitter
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate'
+  }
 
-	-- GitHub Copilot
-	use 'github/copilot.vim'
+  -- GitHub Copilot
+  use 'github/copilot.vim'
 
-	-- Telescope
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
-		requires = { { 'nvim-lua/plenary.nvim' } }
-	}
+  -- Telescope
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use 'nvim-telescope/telescope-file-browser.nvim'
 
-	use { "nvim-telescope/telescope-file-browser.nvim" }
+  -- Rust LSP
+  use 'simrat39/rust-tools.nvim'
 
-	-- Rust LSP
-	use 'simrat39/rust-tools.nvim'
+  -- Pictograms
+  use 'onsails/lspkind.nvim'
 
-	-- Autocomplete
-	use 'hrsh7th/cmp-buffer'
-	use 'hrsh7th/cmp-nvim-lsp'
-	use 'hrsh7th/cmp-nvim-lsp-signature-help'
-	use 'hrsh7th/cmp-path'
-	use 'hrsh7th/cmp-vsnip'
-	use 'hrsh7th/nvim-cmp'
-	use 'hrsh7th/vim-vsnip'
-	use 'hrsh7th/vim-vsnip-integ'
+  -- Autocomplete
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/nvim-cmp'
 end)
