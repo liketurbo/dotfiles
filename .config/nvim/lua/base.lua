d35ee9ed8394802517344cc1726212dbb8772efe
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-22 12:41:11 +0300

    feat: ensure that indentation

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -1,22 +1,19 @@
--- Number lines
-vim.wo.number = true
--- Global clipboard
-vim.opt.clipboard = 'unnamedplus'
+vim.g.mapleader = ' ' -- Leader key
+vim.g.copilot_assume_mapped = true -- Copilot + cmp
+vim.wo.wrap = false -- No wrap
+vim.wo.number = true -- Number lines
+vim.opt.clipboard = 'unnamedplus' -- Global clipboard
+vim.opt.hlsearch = false -- Disable search highlight
+
 -- Tabspace 2
 vim.opt.shiftwidth = 2
--- Leader key
-vim.g.mapleader = ' '
--- No wrap
-vim.wo.wrap = false
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
 
 -- Highlight yank
 vim.api.nvim_command('augroup highlight_yank')
 vim.api.nvim_command('autocmd!')
 vim.api.nvim_command('au TextYankPost * silent! lua vim.highlight.on_yank { higroup=\'IncSearch\', timeout=200 }')
 vim.api.nvim_command('augroup END')
-
--- Disable search highlight
-vim.opt.hlsearch = false
-
--- Copilot + cmp
-vim.g.copilot_assume_mapped = true
