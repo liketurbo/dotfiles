678e1baac9867100ffb862568d06641fb9d5e0c3
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-11-25 01:50:15 +0300

    refactor: remove unused code

--- .config/nvim/after/plugin/cmp.rc.lua
+++ .config/nvim/after/plugin/cmp.rc.lua
@@ -1,15 +1,10 @@
 local cmp = require 'cmp'
 local lspkind = require 'lspkind'
 
-local check_backspace = function()
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-end
-
 cmp.setup {
   snippet = {
     expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
+      vim.fn['vsnip#anonymous'](args.body)
     end,
   },
   mapping = {
@@ -17,7 +12,7 @@ cmp.setup {
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-k>'] = cmp.mapping.select_prev_item(),
     ['<C-j>'] = cmp.mapping.select_next_item(),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 's' }),
+    ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
     ['<CR>'] = cmp.mapping.confirm({
       select = true,
