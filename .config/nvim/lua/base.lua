8ad5626735cba253138e051823684eef707a87bb
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-12-08 19:34:45 +0300

    chore: shorten sequence timeout

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -4,6 +4,8 @@ vim.wo.number = true -- Number lines
 vim.opt.clipboard = 'unnamedplus' -- Global clipboard
 vim.opt.hlsearch = false -- Disable search highlight
 vim.api.nvim_command('au TextYankPost * silent! lua vim.highlight.on_yank { higroup=\'IncSearch\', timeout=200 }') -- Highlight yank
+vim.opt.timeoutlen = 200 -- Time to wait for a mapped sequence to complete (in milliseconds)
+
 
 -- Tabspace 2
 vim.opt.shiftwidth = 2
