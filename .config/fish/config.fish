e13da3c1d311b4a8c3e4435424c70414b293d228
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-12-14 14:45:31 +0300

    chore: add python alias

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,6 +9,7 @@ alias ll="exa -l --icons"
 alias lla="ll -a"
 alias vim="nvim"
 alias cls="clear"
+alias python="python3"
 
 # JK exit
 bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
