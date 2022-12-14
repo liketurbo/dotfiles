ae9981e946ba45a2a60fe5446f8cb28e98848df7
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-12-14 14:46:09 +0300

    chore: add clipboard binding

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,3 +14,8 @@ alias python="python3"
 # JK exit
 bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
 bind -M insert kj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
+
+# Use global clipboard
+bind yy fish_clipboard_copy
+bind Y fish_clipboard_copy
+bind p fish_clipboard_paste
