32f674870d14d9018b9531ea5a18eb98d8eb2561
Rami Chasygov <71ncpt4j6@mozmail.com>
2022-12-17 00:27:48 +0300

    chore: remove JK for fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,10 +11,6 @@ alias vim="nvim"
 alias cls="clear"
 alias python="python3"
 
-# JK exit
-bind -M insert jk "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
-bind -M insert kj "if commandline -P; commandline -f cancel; else; set fish_bind_mode default; commandline -f backward-char force-repaint; end"
-
 # Use global clipboard
 bind yy fish_clipboard_copy
 bind Y fish_clipboard_copy
