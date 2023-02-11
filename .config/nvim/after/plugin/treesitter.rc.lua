7f13d706b76dd54d781aba469d561ba019ee49a9
Rami Chasygov <3c9wjqgw5@mozmail.com>
2023-02-11 22:48:53 +0300

    chore: remove support for func, c++

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -22,24 +22,8 @@ ts.setup {
     "css",
     "html",
     "lua",
-    "c",
-    "cpp",
   },
   autotag = {
     enable = true,
   },
 }
-
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.func = {
-  install_info = {
-    url = "/home/kennym/Projects/tree-sitter-func",
-    files = { "src/parser.c", },
-    branch = "master",
-    generate_requires_npm = false,
-    requires_generate_from_grammar = false,
-  },
-  filetype = "func",
-}
-vim.api.nvim_command [[autocmd BufNewFile,BufRead *.func setfiletype func]]
-vim.api.nvim_command [[autocmd BufNewFile,BufRead *.fc setfiletype func]]
