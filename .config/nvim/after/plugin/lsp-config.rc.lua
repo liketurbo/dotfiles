4a3a846ad09370de8f6d7222d80426b4143f9bc2
Rami Chasygov <3c9wjqgw5@mozmail.com>
2023-03-30 21:46:40 +0300

    refactor: change lsp

--- .config/nvim/after/plugin/lsp-config.rc.lua
+++ .config/nvim/after/plugin/lsp-config.rc.lua
@@ -1,36 +1,34 @@
 local nvim_lsp = require('lspconfig')
 local rt = require('rust-tools')
 
-local lsp_formatting = function(bufnr)
-  vim.lsp.buf.format({
-    filter = function(client)
-      -- apply whatever logic you want (in this example, we'll only use null-ls)
-      return client.name == "null-ls"
-    end,
-    bufnr = bufnr,
-  })
-end
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
 local on_attach = function(client, bufnr)
-  local buf_opts = { noremap = true, silent = true, buffer = bufnr }
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, buf_opts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, buf_opts)
-  vim.keymap.set('n', '<C-Space>', vim.lsp.buf.hover, buf_opts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, buf_opts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, buf_opts)
-  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, buf_opts)
-  vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, buf_opts)
-  vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, buf_opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, buf_opts)
-  vim.keymap.set('n', '<leader>f', vim.lsp.buf.formatting, buf_opts)
-  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, buf_opts)
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, buf_opts)
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, buf_opts)
-  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, buf_opts)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', '<C-Space>', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>a', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 
   local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
   if client.server_capabilities.documentFormattingProvider then
     vim.api.nvim_command [[augroup Format]]
     vim.api.nvim_command [[autocmd! * <buffer>]]
@@ -39,15 +37,23 @@ local on_attach = function(client, bufnr)
   end
 end
 
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+
 nvim_lsp.tsserver.setup {
   on_attach = on_attach,
+  flags = lsp_flags,
   filetypes = { 'javascript', 'typescript', 'typescriptreact', 'typescript.tsx' },
   cmd = { 'typescript-language-server', '--stdio' },
 }
 
 rt.setup {
-    on_attach = on_attach,
-    flags = lsp_flags,
+    server = {
+        on_attach = on_attach,
+        flags = lsp_flags,
+    },
     settings = {
       ["rust-analyzer"] = {}
     }
