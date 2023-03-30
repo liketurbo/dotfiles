487bb98a1c08a20ce53501cbdbbf96b817d247cc
Rami Chasygov <liketurbo.work@gmail.com>
2023-03-30 21:56:36 +0300

    chore: remove jk kj maps

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -3,7 +3,3 @@ vim.keymap.set('n', '<leader>w', ':w<CR>') -- Save
 vim.keymap.set('n', '<leader><leader>', '<C-^>', { noremap = true }) -- Toggle buffers
 vim.keymap.set('i', '<C-v>', '<C-r> <C-r>+', { noremap = true }) -- Paste from clipboard
 vim.keymap.set('i', '<C-z>', '<C-o>u', { noremap = true }) -- Undo in insert mode
-
--- Switch back to normal mode
-vim.keymap.set('i', 'jk', '<Esc>')
-vim.keymap.set('i', 'kj', '<Esc>')
