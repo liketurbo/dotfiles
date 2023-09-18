9272284a95ff752aacdf3d0c426cc02c3d63f6a3
Rami Chasygov <3c9wjqgw5@mozmail.com>
2023-09-18 23:50:06 +0300

    Add darkman scripts

---
+++ .local/share/dark-mode.d/xfce-terminal.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+terminalrc_path="$HOME/.config/xfce4/terminal/terminalrc"
+theme_path="$HOME/.config/xfce4/terminal/colorschemes/solarized-dark.theme"
+
+# Check if both files exist
+if [ ! -f "$terminalrc_path" ] || [ ! -f "$theme_path" ]; then
+  echo "Error: One or both of the files ($terminalrc_path or $theme_path) not found."
+  exit 1
+fi
+
+# Read the property names and values from the theme file into an associative array
+declare -A theme_properties
+while IFS== read -r key value; do
+  theme_properties["$key"]=$value
+done < "$theme_path"
+
+# Function to replace property in the terminalrc file
+replace_property() {
+  local property="$1"
+  local new_value="${theme_properties[$property]}"
+
+  if [ -n "$new_value" ]; then
+    # Escape any backslashes and forward slashes in the value
+    new_value_escaped=$(sed 's/[\/&]/\\&/g' <<< "$new_value")
+    # Replace the property in terminalrc
+    sed -i "s/\($property\s*=\s*\).*/\1$new_value_escaped/" "$terminalrc_path"
+  fi
+}
+
+# List of properties to replace from the theme
+properties_to_replace=(
+  "ColorForeground"
+  "ColorBackground"
+  "ColorCursor"
+  "TabActivityColor"
+  "ColorPalette"
+  "ColorBold"
+)
+
+# Loop through the properties and replace them in terminalrc
+for property in "${properties_to_replace[@]}"; do
+  replace_property "$property"
+done
---
+++ .local/share/dark-mode.d/xfce-wallpaper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI-1/workspace0/last-image -s /usr/share/backgrounds/xfce/xfce-shapes.svg
