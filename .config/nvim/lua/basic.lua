25f02acceb7d4cc67e1ddb21e6f2eff7a5f6e531
Rami Chasygov <3c9wjqgw5@mozmail.com>
2023-10-03 22:00:44 +0300

    Fix nvim order

---
+++ .config/nvim/lua/basic.lua
@@ -0,0 +1,30 @@
+-- Highlight yank
+vim.api.nvim_command('au TextYankPost * silent! lua vim.highlight.on_yank { higroup=\'IncSearch\', timeout=200 }')
+-- Format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+	buffer = buffer,
+	callback = function()
+		vim.lsp.buf.format {}
+	end
+})
+-- Show whitespaces
+vim.opt.list = true
+vim.opt.listchars = 'eol:¬,space:·,tab:··'
+-- Global clipboard
+vim.opt.clipboard = 'unnamedplus'
+-- Number lines
+vim.opt.number = true
+-- No wrap
+vim.opt.wrap = false
+-- Leader key
+vim.g.mapleader = ' '
+-- Treesitter folding
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.cmd("set nofoldenable")
+-- Tab 4 spaces
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.cmd("set noexpandtab")
+vim.opt.timeoutlen = 300
--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -1,7 +1,11 @@
-vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })           -- copy
-vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })     -- copy
-vim.keymap.set('n', '<C-q>', ':q<CR>', { noremap = true, silent = true })           -- quit
-vim.keymap.set('i', '<C-q>', '<Esc>:q<CR>', { noremap = true, silent = true })      -- quit
-vim.keymap.set('n', '<leader><leader>', '<C-^>', { noremap = true, silent = true }) -- toggle buffers
-vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })               -- quit
-vim.keymap.set('i', 'kj', '<Esc>', { noremap = true, silent = true })               -- quit
+-- Copy
+vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })
+-- Quit
+vim.keymap.set('n', '<C-q>', ':q<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-q>', '<Esc>:q<CR>', { noremap = true, silent = true })
+-- Toogle buffers
+vim.keymap.set('n', '<leader><leader>', '<C-^>', { noremap = true, silent = true })
+-- Escape
+vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('i', 'kj', '<Esc>', { noremap = true, silent = true })
