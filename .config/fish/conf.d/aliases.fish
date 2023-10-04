991dffc2669323553cfffaf83997c82be9baea12
Rami Chasygov <liketurbo.work@gmail.com>
2023-10-04 20:56:57 +0300

    New fish aliases

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,8 +1,10 @@
+alias ls="eza --icons"
+alias lsa="eza --icons -a"
 alias ll="eza -l --icons"
 alias lla="eza -l --icons -a"
 alias cls="clear"
 alias vim="nvim"
 function cd
   builtin cd $argv
-  lla
+  lsa
 end
