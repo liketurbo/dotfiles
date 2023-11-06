37e62d88bff43828a9bc3a8d912246f07e8cc9d1
Rami Chasygov <3c9wjqgw5@mozmail.com>
2023-11-06 15:25:22 +0300

    C-q exit without prompt

--- .config/nvim/lua/maps.lua
+++ .config/nvim/lua/maps.lua
@@ -2,8 +2,8 @@
 vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { noremap = true, silent = true })
 -- Quit
-vim.keymap.set("n", "<C-q>", ":q<CR>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-q>", "<Esc>:q<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-q>", ":q!<CR>", { noremap = true, silent = true })
+vim.keymap.set("i", "<C-q>", "<Esc>:q!<CR>", { noremap = true, silent = true })
 -- Toogle buffers
 vim.keymap.set("n", "<leader><leader>", "<C-^>", { noremap = true, silent = true })
 -- Escape
