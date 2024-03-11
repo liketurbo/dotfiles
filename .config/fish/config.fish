132245b7f26773f7b59993835fe0e12d98aa1ee5
Rami Chasygov <liketurbo.work@gmail.com>
2024-03-11 20:03:10 +0300

    Add pnpm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,17 @@
-# No greeting
+# no greeting
 set fish_greeting
-# Use vi
+
+# use vi
 fish_vi_key_bindings
 bind -M insert -m default jk repaint
 bind -M insert -m default kj repaint
 
-# Env variables
+# rclone
 set -x RCLONE_PASSWORD_COMMAND "pass rclone/config"
+
+# pnpm
+set -gx PNPM_HOME "/home/kennym/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
