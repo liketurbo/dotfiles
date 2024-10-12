fb726ea613e03ee27928730924cce928cb1c0723
Rami Chasygov <liketurbo.work@gmail.com>
2024-10-12 09:30:07 +0300

    Display and tmux setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,7 @@
 # no greeting
 set fish_greeting
 
-# rclone
+# passless rclone
 set -x RCLONE_PASSWORD_COMMAND "pass rclone/config"
 
 # pnpm
@@ -11,5 +11,12 @@ if not string match -q -- $PNPM_HOME $PATH
 end
 # pnpm end
 
-# remap caps lock to esc
-setxkbmap -option caps:escape
+# show display
+if test -z "$DISPLAY" && test (tty) = "/dev/tty1"
+  startx
+end
+
+# start tmux
+if test "$DISPLAY" && test -z "$TMUX"
+  exec tmux
+end
