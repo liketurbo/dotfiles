e7d05557e18981f7b6e530af6488dabf43da37d4
Rami Chasygov <liketurbo.work@gmail.com>
2025-01-04 17:45:20 +0300

    Name session

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,13 +4,6 @@ set fish_greeting
 # passless rclone
 set -x RCLONE_PASSWORD_COMMAND "pass rclone/config"
 
-# pnpm
-set -gx PNPM_HOME "/home/kennym/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
 # show display
 if test -z "$DISPLAY" && test (tty) = "/dev/tty1"
   startx
@@ -18,5 +11,16 @@ end
 
 # start tmux
 if test "$DISPLAY" && test -z "$TMUX"
-  exec tmux
+  if tmux has-session -t terminal 2>/dev/null
+	exec tmux attach-session -t terminal
+  else
+	exec tmux new-session -s terminal
+  end
 end
+
+# pnpm
+set -gx PNPM_HOME "/home/kennym/snap/code/174/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
