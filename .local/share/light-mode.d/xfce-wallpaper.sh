b4a6497389a2c5330d5b3ec4b9d5e23e43139827
Rami Chasygov <liketurbo.work@gmail.com>
2025-01-04 17:42:58 +0300

    Fix light wallpaper

--- .local/share/light-mode.d/xfce-wallpaper.sh
+++ .local/share/light-mode.d/xfce-wallpaper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI-1/workspace0/last-image -s /usr/share/backgrounds/xfce/xfce-teal.jpg
+exec xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI-1/workspace0/last-image -s /usr/share/backgrounds/xfce/xfce-teal.svg
