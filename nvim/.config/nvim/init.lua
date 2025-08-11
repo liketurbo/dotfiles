28d169210baa8e3cdc9bb8e56e002e0feb621f95
Rami Chasygov <liketurbo.work@gmail.com>
2025-08-11 15:36:41 +0300

    Support not latin commands

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -24,8 +24,17 @@ vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-q>', ':q<CR>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-q>', '<Esc>:q<CR>', { noremap = true, silent = true })
 
+-- keymap: force quit with Ctrl+Q then q
+vim.keymap.set('n', '<C-S-q>', ':q!<CR>', { noremap = true, silent = true })
+vim.keymap.set('i', '<C-S-q>', '<Esc>:q!<CR>', { noremap = true, silent = true })
+
 -- do not keep highlights after search
 vim.opt.hlsearch = false
 
 -- enable system clipboard
 vim.opt.clipboard = "unnamedplus"
+
+-- make Cyrillic keys act as Latin keys in Normal/Visual/Operator-pending modes
+vim.opt.langmap =
+    'фa,иb,сc,вd,уe,аf,пg,рh,шi,оj,лk,дl,ьm,тn,щo,зp,йq,кr,ыs,еt,гu,мv,цw,чx,нy,яz,' ..
+    'ФA,ИB,СC,ВD,УE,АF,ПG,РH,ШI,ОJ,ЛK,ДL,ЬM,ТN,ЩO,ЗP,ЙQ,КR,ЫS,ЕT,ГU,МV,ЦW,ЧX,НY,ЯZ'
